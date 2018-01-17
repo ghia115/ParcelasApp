cbc3c707017b1c4d53f6eaa920204d882398b808
ghia115 <casaghia115@gmail.com>
2018-01-17 16:27:18 -0600

    separar peticiones de api

---
+++ app/src/main/java/com/example/luis/parcelasapp/GetApi.java
@@ -0,0 +1,114 @@
+package com.example.luis.parcelasapp;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.luis.parcelasapp.modelo.DdsBalance;
+import com.example.luis.parcelasapp.modelo.MresumenRiego;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by mario on 17/01/18.
+ */
+
+public class GetApi {
+
+    public ArrayList<MresumenRiego> apiRiego(){
+        final ArrayList<MresumenRiego> riego = new ArrayList<>();
+        final RequestQueue queue = Volley.newRequestQueue(NoContext.getAppContext());
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                "http://172.16.1.180/app/api/GraficaRiego?est=19&fechaIni=1/04/2017&fechaFin=10/07/2017&opc=1&riego=1&asiento=2",
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        //Log.d("JSON", "Respuesta" + response);
+
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+                                riego.add(resumenRiego(response.getJSONObject(i)));
+                            }
+                            catch (JSONException e) {
+                            }
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Error", "Respuesta en JSON" + error.getMessage());
+                    }
+                }
+        );
+        queue.add(jsonArrayRequest);
+
+        return riego;
+    }
+
+    private final MresumenRiego resumenRiego(JSONObject obj) throws JSONException {
+        String condicion = obj.getString("Condicion");
+        String fecha = obj.getString("Fecha");
+        double lb = obj.getDouble("Lb");
+        double tr = obj.getDouble("Tr");
+
+        return new MresumenRiego(condicion, fecha, lb, tr);
+    }
+
+    public ArrayList<DdsBalance> apiGrfica(){
+        final ArrayList<DdsBalance> riego = new ArrayList<>();
+        final RequestQueue queue = Volley.newRequestQueue(NoContext.getAppContext());
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                "http://172.16.1.180/app/api/GraficaRiego?est=19&fechaIni=1/04/2017&fechaFin=10/07/2017&opc=1&riego=1&asiento=2",
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        //Log.d("JSON", "Respuesta" + response);
+
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+                                riego.add(dataBalance(response.getJSONObject(i)));
+                            }
+                            catch (JSONException e) {
+                            }
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Error", "Respuesta en JSON" + error.getMessage());
+                    }
+                }
+        );
+        queue.add(jsonArrayRequest);
+
+        return riego;
+    }
+
+    private final DdsBalance dataBalance(JSONObject obj) throws JSONException {
+        double balance = obj.getDouble("Balance");
+        int dds = obj.getInt("Dds");
+
+        return new DdsBalance(balance, dds);
+    }
+
+}
--- app/src/main/java/com/example/luis/parcelasapp/fragments/chartFragment.java
+++ app/src/main/java/com/example/luis/parcelasapp/fragments/chartFragment.java
@@ -16,6 +16,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.luis.parcelasapp.GetApi;
 import com.example.luis.parcelasapp.R;
 import com.example.luis.parcelasapp.modelo.DdsBalance;
 import com.jjoe64.graphview.GraphView;
@@ -37,7 +38,8 @@ import java.util.Arrays;
  */
 public class chartFragment extends Fragment {
 
-    final ArrayList<DdsBalance> result = new ArrayList<>();
+    ArrayList<DdsBalance> result = new ArrayList<>();
+    GetApi api = new GetApi();
 
     public chartFragment() {
         // Required empty public constructor
@@ -54,51 +56,27 @@ public class chartFragment extends Fragment {
 
         final GraphView graph = (GraphView) v.findViewById(R.id.graphic);
 
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
-                Request.Method.GET,
-                "http://172.16.1.180/app/api/GraficaRiego?est=19&fechaIni=1/04/2017&fechaFin=10/07/2017&opc=1&riego=1&asiento=2",
-                null,
-                new Response.Listener<JSONArray>() {
-                    @Override
-                    public void onResponse(JSONArray response) {
-                        //Log.d("JSON", "Respuesta" + response);
+        result = api.apiGrfica();
 
-                        for (int i = 0; i < response.length(); i++) {
-                            try {
-                                result.add(dataBalance(response.getJSONObject(i)));
-                            }
-                            catch (JSONException e) {
-                            }
-                        }
-                        LineGraphSeries<DataPoint> series = new LineGraphSeries<>();
-                        for (int i=0; i<result.size(); i++) {
-                            DataPoint point = new DataPoint(result.get(i).getDds(), result.get(i).getBalance());
-                            series.appendData(point, true, result.size());
-                        }
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>();
+        for (int i=0; i<result.size(); i++) {
+            DataPoint point = new DataPoint(result.get(i).getDds(), result.get(i).getBalance());
+            series.appendData(point, true, result.size());
+        }
 
-                        graph.addSeries(series);
-                        graph.setBackgroundColor(Color.argb(50, 160, 160, 160));
-                        series.setColor(Color.CYAN);
-                        series.setAnimated(true);
-                        series.setDrawDataPoints(true);
-                        series.setDataPointsRadius(5);
+        graph.addSeries(series);
+        graph.setBackgroundColor(Color.argb(50, 160, 160, 160));
+        series.setColor(Color.CYAN);
+        series.setAnimated(true);
+        series.setDrawDataPoints(true);
+        series.setDataPointsRadius(5);
 
-                        series.setOnDataPointTapListener(new OnDataPointTapListener() {
-                            @Override
-                            public void onTap(Series series, DataPointInterface dataPoint) {
-                                Toast.makeText(getActivity(), "Dds y Balance: "+dataPoint, Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.d("Error", "Respuesta en JSON" + error.getMessage());
-                    }
-                }
-        );
-        queue.add(jsonArrayRequest);
+        series.setOnDataPointTapListener(new OnDataPointTapListener() {
+            @Override
+            public void onTap(Series series, DataPointInterface dataPoint) {
+                Toast.makeText(getActivity(), "Dds y Balance: "+dataPoint, Toast.LENGTH_SHORT).show();
+            }
+        });
 
         //GraphView graph = (GraphView) v.findViewById(R.id.graphic);
         /*LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
